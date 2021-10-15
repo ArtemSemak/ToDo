47bdd0b492aa622ec183aa7bec9c2401f7f5250b
Artem <artemsemak2000@gmail.com>
2021-10-15 18:23:31 +0300

    addeв functions sort, add, delete

--- src/App.js
+++ src/App.js
@@ -1,38 +1,129 @@
 import './App.css';
 import TDCard from './TDCard';
+import { useState } from 'react'
 
 function App() {
+  let id = 4
+  const date = new Date()
+  const [todos, setTodos] = useState([])
+  let [sortedTodos, setSortedTodos] = useState([])
+  const [value, setValue] = useState('')
+  
+  
+  
+  
+  function sortByDate(flag) {
+    if (flag === 'up') {
+     
+      const list = sortedTodos.sort((a, b) => b.id - a.id)
+      let newList = []
+      Object.assign(newList, list)
+      setSortedTodos(newList)
+    } else { 
+      const list = sortedTodos.sort((a, b) => a.id - b.id)
+      let newList = []
+      Object.assign(newList, list)
+      setSortedTodos(newList)
+    }
+
+
+  }
+
+  function sortByComplete(flag) {
+    
+    if (flag === 'all') {
+      setSortedTodos(todos)
+    }
+    else {
+      setSortedTodos(todos.filter(it => it.completed === flag))
+    }
+
+
+  }
+
+
+  function doneTodo(id) {
+    setTodos(
+      todos.map( todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      }
+      )
+    ) 
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+    setSortedTodos(sortedTodos.filter(todo => todo.id !== id))
+  }
+
+
+  function addTodo(event) {
+    
+    event.preventDefault()
+    
+      if (value.trim()) {
+        console.log(value)
+        setTodos(todos.concat(
+          {
+            id: Date.now(),
+            title: value,
+            completed: false,
+            date: `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
+    
+          }
+        ))
+        setSortedTodos(todos.concat(
+          {
+            id: Date.now(),
+            title: value,
+            completed: false,
+            date: `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
+    
+          }
+        ))
+        
+        setValue(' ')
+      }
+    }
+
+  
+
   return (
-    <div classNameName="App">
+    <div className="App">
     <body>
       <header>
         <section>
             <p className="title">ToDo</p>
         </section>
         <section>
-            <input type="text" placeholder="I want to do..." className="inpTD" title="Write your plan here"/>
-            <input type="button" className="btnAdd" value="Add" title="Add your plan"/>
+          <form onSubmit={addTodo}>
+            <input type="text" value={value} onChange={event => setValue(event.target.value)} placeholder="I want to do..." className="inpTD" title="Write your plan here"/>
+            <button type="submit"  className="btnAdd" title="Add your plan">Add</button>
+          </form> 
         </section>
         <section className="control">
             <section className="dund">
-                <input type="button" value="All" title="Show all plans"/>
-                <input type="button" value="Done" title="Show completed plans"/>
-                <input type="button" value="Undone" className="btnU" title="Show uncompleted plans"/>
+                <input type="button" onClick={() => sortByComplete('all')} value="All" title="Show all plans"/>
+                <input type="button" onClick={() => sortByComplete(true)} value="Done" title="Show completed plans"/>
+                <input type="button" onClick={() => sortByComplete(false)} value="Undone" className="btnU" title="Show uncompleted plans"/>
             </section>
             <section className="dund">
                 <span className="lblar"> Sort by date </span>
-                    <input className="btnD" type="image" src="premium-icon-up-arrow-3987238.png" title="New first"/>
+                    <input className="btnD" onClick={() => sortByDate('up')} type="image" src="premium-icon-up-arrow-3987238.png" title="New first"/>
                 
-                    <input className="btnD" type="image" src="reverse.png" title="Old first"/>
+                    <input className="btnD" onClick={() => sortByDate('down')} type="image" src="reverse.png" title="Old first"/>
                 
             </section>
         </section>
     </header>
 
     <main className="tds">
-      <TDCard tdtext ='todo'  date = '12.23.2001'/>
-      <TDCard tdtext ='todo'  date = '12.23.2001'/>
-      <TDCard tdtext ='todo'  date = '12.23.2001'/>
+      { sortedTodos.length ?  sortedTodos.map( todo => {
+        return <TDCard todo={todo} onChange={doneTodo} deleteTodo={removeTodo}/>
+      }) : <p className="noTD">You dont have ToDos</p>}
     </main>
 
     
--- src/TDCard.js
+++ src/TDCard.js
@@ -1,17 +1,23 @@
 import './App.css';
 
-function TDCard(props) {
+function TDCard({ todo, onChange, deleteTodo }) {
+  const classes = []
+  if (todo.completed) {
+    classes.push('done')
+  }
+
     return (
         <section className="td">
         <section>
             <span><label className="hide">
-                <input type='checkbox'/>
+                <input type='checkbox' onChange={ () => onChange(todo.id) } checked={ todo.completed }/>
                 <span></span>
-                <label className="tdtxt"> { props.tdtext } </label>
+                <label className={ classes.join(' ') }> { todo.title } </label>
               </label> </span>
         </section>
         <section>
-          <label className="lbltext"> { props.date } </label><input type="image" className="btnD trash" src="premium-icon-trash-can-4914888.png" title="Delete plan"/>
+          <label className="lbltext"> { todo.date } </label>
+          <input type="image" className="btnD trash" src="premium-icon-trash-can-4914888.png" title="Delete plan" onClick={ () => deleteTodo(todo.id) }/>
       </section>
     </section>
     )
